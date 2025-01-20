737557b3682aa8e1d72bca79ad7915a56ce2d91c
Nisha Shetty <109810083+nishashetty11@users.noreply.github.com>
2025-01-20 15:29:17 +0530

    Updated Slave Driver Bfm.

--- src/hdlTop/slaveAgentBfm/AhbSlaveDriverBfm.sv
+++ src/hdlTop/slaveAgentBfm/AhbSlaveDriverBfm.sv
@@ -11,18 +11,24 @@ import AhbGlobalPackage::*;
 //  Used as the HDL driver for ahb
 //  It connects with the HVL driver_proxy for driving the stimulus
 //--------------------------------------------------------------------------------------------
-interface AhbSlaveDriverBfm (input bit pclk,
-                               input bit preset_n,
-                               input bit [NO_OF_SLAVES-1:0]psel,
-                               input logic penable,
-                               input logic [ADDRESS_WIDTH-1:0]paddr,
-                               input logic pwrite,
-                               input logic [(DATA_WIDTH/8)-1:0]pstrb, 
-                               input logic [DATA_WIDTH-1:0]pwdata,
-                               output bit pslverr,
-                               output bit pready,
-                               input bit [2:0]pprot,
-                               output logic [DATA_WIDTH-1:0]prdata
+interface AhbSlaveDriverBfm (input  bit   HCLK,
+                              input  bit  HRESETn,
+                              input logic [ADDR_WIDTH-1:0] HADDR;
+                              input logic [2:0] HBURST;
+                              input logic HMASTLOCK;
+                              input logic [HPROT_WIDTH-1:0] HPROT;
+                              input logic [2:0] HSIZE;
+                              input logic HNONSEC;
+                              input logic HEXCL;
+                              input logic [HMASTER_WIDTH-1:0] HMASTER;
+                              input logic [1:0] HTRANS;     
+                              input logic HWRITE;
+                              input logic [DATA_WIDTH-1:0] HWDATA;
+                              output logic [DATA_WIDTH-1:0] HRDATA;
+                              output logic HREADY;
+                              output logic HREADYOUT;
+                              output logic HRESP;
+                              output logic HEXOKAY;
                                );
 
   //-------------------------------------------------------
@@ -35,14 +41,17 @@ interface AhbSlaveDriverBfm (input bit pclk,
   // Importing slave driver proxy
   //------------------------------------------------------- 
   import AhbSlavePackage::*;
-
-  //Variable: ahb_slave_drv_proxy_h
-  //Declaring handle for AhbSlaveDriverProxy
-  AhbSlaveDriverProxy ahb_slave_drv_proxy_h;
   
   //Variable : name
   //Used to store the name of the interface
   string name = "AHB_SLAVE_DRIVER_BFM";
+  
+
+  //Variable: ahb_slave_drv_proxy_h
+  //Declaring handle for AhbSlaveDriverProxy
+  
+  AhbSlaveDriverProxy ahb_slave_drv_proxy_h;
+  
 
   //-------------------------------------------------------
   // Used to display the name of the interface
@@ -52,61 +61,24 @@ interface AhbSlaveDriverBfm (input bit pclk,
   end
 
   //-------------------------------------------------------
-  // Task: wait_for_preset_n
+  // Task: wait_for_HRESETn
   // Waiting for the system reset to be active low
   //-------------------------------------------------------
-  task wait_for_preset_n();
+  task wait_for_HRESETn();
 
-    @(negedge preset_n);
+    @(negedge HRESETn);
     `uvm_info(name,$sformatf("SYSTEM RESET DETECTED"),UVM_HIGH)
-    @(posedge preset_n);
+    @(posedge HRESETn);
     `uvm_info(name,$sformatf("SYSTEM RESET DEACTIVATED"),UVM_HIGH)
   
-  endtask: wait_for_preset_n
+  endtask: wait_for_HRESETn
   
   //-------------------------------------------------------
   // Task: wait_for_setup_state
   // Samples the required data and sends back to the proxy
   //-------------------------------------------------------
   task wait_for_setup_state(output ahb_transfer_char_s data_packet);
-    @(posedge pclk);
-    
-    `uvm_info(name,$sformatf("WAITING FOR SETUP STATE"),UVM_HIGH)
-    `uvm_info(name,$sformatf("PSEL=%0d",psel),UVM_HIGH)
-    //`uvm_info(name,$sformatf("LOCAL_SLAVE_ID=%0d",slave_id),UVM_HIGH)
-    
-    // MSHA: while(psel == 1'bx) begin
-    // MSHA:   `uvm_info(name,$sformatf("WAITING FOR SETUP STATE-INSIDE WHILE LOOP"),UVM_HIGH)
-    // MSHA:   `uvm_info(name,$sformatf("PSEL=%0d",psel),UVM_HIGH)
-    // MSHA:   @(posedge pclk);
-    // MSHA: end
-    
-    while(psel[0] !==1) begin
-      `uvm_info(name, $sformatf("Inside while loop: penable =%0d, pready=%0d, psel=%0d ", penable, pready, psel), UVM_HIGH)
-      @(negedge pclk);
-    end
-    //while(penable != 1'b0) begin
-    //  `uvm_info(name,$sformatf("WAITING FOR SETUP STATE-INSIDE WHILE LOOP"),UVM_HIGH)
-    //  `uvm_info(name,$sformatf("PSEL=%0d",psel),UVM_HIGH)
-    //  @(posedge pclk);
-    //end
-
-    `uvm_info(name,$sformatf("SETUP PHASE STARTED"),UVM_HIGH)
-    `uvm_info(name,$sformatf("PSEL=%0d",psel),UVM_HIGH)
-
-    // Sampling the signals
-    data_packet.pselx  = psel;
-    data_packet.paddr  = paddr;
-    data_packet.pwrite = pwrite;
-    if(pwrite == WRITE) begin
-      data_packet.pwdata = pwdata;
-      data_packet.pstrb  = pstrb;
-    end
-    data_packet.pprot = pprot;
-
-    // TODO(mshariff): 
-    // Get the required READ data and PSLVERR
-
+  //logic
   endtask: wait_for_setup_state
 
   //-------------------------------------------------------
@@ -115,27 +87,7 @@ interface AhbSlaveDriverBfm (input bit pclk,
   // on pwrite signal
   //-------------------------------------------------------
   task wait_for_access_state(inout ahb_transfer_char_s data_packet);
-    @(posedge pclk);
-    `uvm_info(name,$sformatf("WAITING FOR ACCESS STATE - no_of_wait_states=%0d",data_packet.no_of_wait_states),UVM_HIGH);
-
-    repeat(data_packet.no_of_wait_states)begin
-      `uvm_info(name,$sformatf("INSIDE ACCESS - DRIVING WAIT STATE"),UVM_HIGH);
-      @(posedge pclk);
-      pready<=0;
-    end
-    pready<=1;
-
-    // This display checks whether the data from proxy is received or not
-    `uvm_info(name,$sformatf("INSIDE ACCESS - PRDATA=%0h",data_packet.prdata),UVM_HIGH);
-    
-    if(data_packet.pwrite == READ) begin
-      `uvm_info(name,$sformatf("INSIDE ACCESS - PRDATA=%0h",data_packet.prdata),UVM_HIGH);
-      prdata <= data_packet.prdata;
-    end
-
-    // TODO(mshariff): 
-    pslverr <= data_packet.pslverr;
-
+   //logic
   endtask: wait_for_access_state
 
 endinterface : AhbSlaveDriverBfm
