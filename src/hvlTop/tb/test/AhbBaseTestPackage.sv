9a3ba369a9032d8d2278384353802c9414d371b0
Sonal J Ail <131892960+sonalail@users.noreply.github.com>
2025-01-20 15:07:15 +0530

    Created AhbBaseTestPackage.sv

---
+++ src/hvlTop/tb/test/AhbBaseTestPackage.sv
@@ -0,0 +1,34 @@
+`ifndef AHB_BASE_TEST_PKG_INCLUDED_
+`define AHB_BASE_TEST_PKG_INCLUDED_
+
+//-----------------------------------------------------------------------------------------
+// Package: ahb base_test
+//  Includes all the files written to run the simulation
+//--------------------------------------------------------------------------------------------
+package AhbBaseTestPackage;
+
+  //-------------------------------------------------------
+  // Import uvm package
+  //-------------------------------------------------------
+  `include "uvm_macros.svh"
+  import uvm_pkg::*;
+
+  //-------------------------------------------------------
+  // Importing the required packages
+  //-------------------------------------------------------
+  import AhbGlobalPackage::*;
+  import AhbMasterPackage::*;
+  import AhbSlavePackage::*;
+  import AhbEnvironmentPackage::*;
+  import AhbMasterSequencePackage::*;
+  import AhbSlaveSequencePackage::*;
+  import AhbVirtualSequencePackage::*;
+  
+  //-------------------------------------------------------
+  // Including the base test files
+  //-------------------------------------------------------
+
+
+endpackage : AhbBaseTestPackage
+
+`endif
