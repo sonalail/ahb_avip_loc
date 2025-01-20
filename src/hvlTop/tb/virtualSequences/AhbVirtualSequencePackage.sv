f175f929e35abfd08494a6717a41553704949c7a
Sonal J Ail <131892960+sonalail@users.noreply.github.com>
2025-01-20 15:48:31 +0530

    modified package file

--- src/hvlTop/tb/virtualSequences/AhbVirtualSequencePackage.sv
+++ src/hvlTop/tb/virtualSequences/AhbVirtualSequencePackage.sv
@@ -1,1 +1,29 @@
+`ifndef AHB_VIRTUAL_SEQ_PKG_INCLUDED_
+`define AHB_VIRTUAL_SEQ_PKG_INCLUDED_
 
+//--------------------------------------------------------------------------------------------
+// Package : AhbVirtualSequencePackage
+//  Includes all the master seq files declared
+//--------------------------------------------------------------------------------------------
+package AhbVirtualSequencePackage;
+
+  //-------------------------------------------------------
+  // Importing UVM Pkg
+  //-------------------------------------------------------
+  `include "uvm_macros.svh"
+  import AhbGlobalPackage::*;
+  import AhbMasterPackage::*;
+  import AhbSlavePackage::*;
+  import AhbEnvironmentPackage::*;
+  import AhbMasterSequencePackage::*;
+  import AhbSlaveSequencePackage::*;
+  import AhbVirtualSequencePackage::*;
+
+  //-------------------------------------------------------
+  // Including required seq files
+  //-------------------------------------------------------
+  `include "AhbVirtualBaseSequence.sv"
+  
+endpackage : apb_virtual_seq_pkg
+
+`endif
