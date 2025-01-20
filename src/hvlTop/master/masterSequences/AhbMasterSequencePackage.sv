1eb1894fba5b3af588983ffa170d6cd2be746029
Sonal J Ail <131892960+sonalail@users.noreply.github.com>
2025-01-20 15:34:03 +0530

    Updated AhbMasterSequencePackage  file

--- src/hvlTop/master/masterSequences/AhbMasterSequencePackage.sv
+++ src/hvlTop/master/masterSequences/AhbMasterSequencePackage.sv
@@ -1,1 +1,25 @@
+`ifndef AHB_MASTER_SEQ_PKG_INCLUDED_
+`define AHB_MASTER_SEQ_PKG_INCLUDED_
 
+//--------------------------------------------------------------------------------------------
+// Package : apb_master_seq_pkg
+//  Includes all the master seq files declared
+//--------------------------------------------------------------------------------------------
+package AhbMasterSequencePackage;
+
+  //-------------------------------------------------------
+  // Importing UVM Pkg and including globall and master packages
+  //-------------------------------------------------------
+  import uvm_pkg::*;
+  `include "uvm_macros.svh"
+  import AhbGlobalPackage::*;
+  import AhbMasterPackage::*;
+
+  //-------------------------------------------------------
+  // Including required ahb master seq files
+  //-------------------------------------------------------
+  `include "AhbMasterBaseSequence.sv"
+
+endpackage : AhbMasterSequencePackage
+
+`endif
