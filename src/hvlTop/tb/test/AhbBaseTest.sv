62430a401e5ba5cbd25db5cd3a722c25e1427ee3
Gyanappa dalavai <138701830+Gyanappadalavai@users.noreply.github.com>
2025-01-21 12:23:40 +0530

    Update AhbBaseTest.sv

--- src/hvlTop/tb/test/AhbBaseTest.sv
+++ src/hvlTop/tb/test/AhbBaseTest.sv
@@ -1,5 +1,5 @@
-`ifndef AHB_BASE_TEST_INCLUDED_
-`define AHB_BASE_TEST_INCLUDED_
+`ifndef AHBBASETEST_INCLUDED_
+`define AHBBASETEST_INCLUDED_
 
 
 //--------------------------------------------------------------------------------------------
@@ -13,20 +13,20 @@ class AhbBaseTest extends uvm_test;
   
   //Variable: env_h
   //Declaring a handle for env
-  AhbEnvironment ahb_env_h;
+  AhbEnvironment ahbEnvironment;
 
-  //Variable: ahb_env_cfg_h
+  //Variable: ahbEnvironmentConfig
   //Declaring a handle for env_cfg_h
-  AhbEnvironmentConfig ahb_env_cfg_h;
+  AhbEnvironmentConfig ahbEnvironmentConfig;
 
   //-------------------------------------------------------
   // Externally defined Tasks and Functions
   //-------------------------------------------------------
   extern function new(string name = "AhbBaseTest", uvm_component parent = null);
   extern virtual function void build_phase(uvm_phase phase);
-  extern virtual function void setup_AhbEnvironmentConfig();
-  extern virtual function void setup_AhbMasterAgentConfig();
-  extern virtual function void setup_AhbSlaveAgentConfig();
+  extern virtual function void setupAhbEnvironmentConfig();
+  extern virtual function void setupAhbMasterAgentConfig();
+  extern virtual function void setupAhbSlaveAgentConfig();
   extern virtual function void end_of_elaboration_phase(uvm_phase phase);
   extern virtual task run_phase(uvm_phase phase);
 
@@ -52,33 +52,33 @@ endfunction : new
 //--------------------------------------------------------------------------------------------
 function void AhbBaseTest::build_phase(uvm_phase phase);
   super.build_phase(phase);
-  setup_AhbEnvironmentConfig();
-  ahb_env_h = AhbEnvironment::type_id::create("AhbEnvironment",this);
+  setupAhbEnvironmentConfig();
+  ahbEnvironment = AhbEnvironment::type_id::create("ahbEnvironment",this);
 endfunction : build_phase
 
 //--------------------------------------------------------------------------------------------
 // Function : setup_AhbEnvironmentConfig
 //  It calls the master agent config setup and slave agent config steup functions
 //--------------------------------------------------------------------------------------------
-function void AhbBaseTest::setup_AhbEnvironmentConfig();
-  ahb_env_cfg_h = AhbEnvironmentConfig::type_id::create("ahb_env_cfg_h");
-  ahb_env_cfg_h.no_of_slaves      = NO_OF_SLAVES;
-  ahb_env_cfg_h.has_scoreboard    = 1;
-  ahb_env_cfg_h.has_virtual_seqr  = 1;
+function void AhbBaseTest::setupAhbEnvironmentConfig();
+  ahbEnvironmentConfig = AhbEnvironmentConfig::type_id::create("ahbEnvironmentConfig");
+  ahbEnvironmentConfig.no_of_slaves      = NO_OF_SLAVES;
+  ahbEnvironmentConfig.has_scoreboard    = 1;
+  ahbEnvironmentConfig.has_virtual_seqr  = 1;
 
   //Setting up the configuration for master agent
   setup_AhbMasterAgentConfig();
 
   //Setting the master agent configuration into config_db
    uvm_config_db#(AhbMasterAgentConfig)::set(this,"*master_agent*","AhbMasterAgentConfig",
-                                               ahb_env_cfg_h.ahb_master_agent_cfg_h);
+                                               ahbEnvironmentConfig.ahbMasterAgentConfig);
  //Displaying the master agent configuration
-  `uvm_info(get_type_name(),$sformatf("\nAHB_MASTER_AGENT_CONFIG\n%s",ahb_env_cfg_h.ahb_master_agent_cfg_h.sprint()),UVM_LOW);
+  `uvm_info(get_type_name(),$sformatf("\nAHB_MASTER_AGENT_CONFIG\n%s",ahbEnvironmentConfig.ahbMasterAgentConfig.sprint()),UVM_LOW);
 
-  setup_AhbSlaveAgentConfig();
+  setupAhbSlaveAgentConfig();
 
-  uvm_config_db#(AhbEnvironmentConfig)::set(this,"*","AhbEnvironmentConfig",ahb_env_cfg_h);
-  `uvm_info(get_type_name(),$sformatf("\nAHB_ENV_CONFIG\n%s",ahb_env_cfg_h.sprint()),UVM_LOW);
+  uvm_config_db#(AhbEnvironmentConfig)::set(this,"*","AhbEnvironmentConfig",ahbEnvironmentConfig);
+  `uvm_info(get_type_name(),$sformatf("\nAHB_ENV_CONFIG\n%s",ahbEnvironmentConfig.sprint()),UVM_LOW);
 
 endfunction : setup_AhbEnvironmentConfig
 //--------------------------------------------------------------------------------------------
@@ -87,35 +87,35 @@ endfunction : setup_AhbEnvironmentConfig
 //  Creates the master agent config
 //  Sets ahb master agent config into configdb 
 //--------------------------------------------------------------------------------------------
-function void AhbBaseTest::setup_AhbMasterAgentConfig();
+function void AhbBaseTest::setupAhbMasterAgentConfig();
   bit [63:0]local_min_address;
   bit [63:0]local_max_address;
   
-  ahb_env_cfg_h.ahb_master_agent_cfg_h = AhbMasterAgentConfig::type_id::create("AhbMasterAgentConfig");
+  ahbEnvironmentConfig.ahbMasterAgentConfig = AhbMasterAgentConfig::type_id::create("AhbMasterAgentConfig");
   
   if(MASTER_AGENT_ACTIVE === 1) begin
-    ahb_env_cfg_h.ahb_master_agent_cfg_h.is_active = uvm_active_passive_enum'(UVM_ACTIVE);
+    ahbEnvironmentConfig.ahbMasterAgentConfig.is_active = uvm_active_passive_enum'(UVM_ACTIVE);
   end
   else begin
-    ahb_env_cfg_h.ahb_master_agent_cfg_h.is_active = uvm_active_passive_enum'(UVM_PASSIVE);
+    ahbEnvironmentConfig.ahbMasterAgentConfig.is_active = uvm_active_passive_enum'(UVM_PASSIVE);
   end
-  ahb_env_cfg_h.ahb_master_agent_cfg_h.no_of_slaves = NO_OF_SLAVES;
-  ahb_env_cfg_h.ahb_master_agent_cfg_h.has_coverage = 1;
+  ahbEnvironmentConfig.ahbMasterAgentConfig.no_of_slaves = NO_OF_SLAVES;
+  ahbEnvironmentConfig.ahbMasterAgentConfig.has_coverage = 1;
 
   for(int i =0; i<NO_OF_SLAVES; i++) begin
     if(i == 0) begin  
-      ahb_env_cfg_h.ahb_master_agent_cfg_h.master_min_addr_range(i,0);
-      local_min_address = ahb_env_cfg_h.ahb_master_agent_cfg_h.master_min_addr_range_array[i];
+      ahbEnvironmentConfig.ahbMasterAgentConfig.master_min_addr_range(i,0);
+      local_min_address = ahbEnvironmentConfig.ahbMasterAgentConfig.master_min_addr_range_array[i];
       
-      ahb_env_cfg_h.ahb_master_agent_cfg_h.master_max_addr_range(i,2**(SLAVE_MEMORY_SIZE)-1 );
-      local_max_address = ahb_env_cfg_h.ahb_master_agent_cfg_h.master_max_addr_range_array[i];
+      ahbEnvironmentConfig.ahbMasterAgentConfig.master_max_addr_range(i,2**(SLAVE_MEMORY_SIZE)-1 );
+      local_max_address = ahbEnvironmentConfig.ahbMasterAgentConfig.master_max_addr_range_array[i];
     end
     else begin
-      ahb_env_cfg_h.ahb_master_agent_cfg_h.master_min_addr_range(i,local_max_address + SLAVE_MEMORY_GAP);
-      local_min_address = ahb_env_cfg_h.ahb_master_agent_cfg_h.master_min_addr_range_array[i];
+      ahbEnvironmentConfig.ahbMasterAgentConfig.master_min_addr_range(i,local_max_address + SLAVE_MEMORY_GAP);
+      local_min_address = ahbEnvironmentConfig.ahbMasterAgentConfig.master_min_addr_range_array[i];
       
-      ahb_env_cfg_h.ahb_master_agent_cfg_h.master_max_addr_range(i,local_max_address+2**(SLAVE_MEMORY_SIZE)-1 + SLAVE_MEMORY_GAP);
-      local_max_address = ahb_env_cfg_h.ahb_master_agent_cfg_h.master_max_addr_range_array[i];
+      ahbEnvironmentConfig.ahbMasterAgentConfig.master_max_addr_range(i,local_max_address+2**(SLAVE_MEMORY_SIZE)-1 + SLAVE_MEMORY_GAP);
+      local_max_address = ahbEnvironmentConfig.ahbMasterAgentConfig.master_max_addr_range_array[i];
     end
   end
 endfunction : setup_AhbMasterAgentConfig
@@ -125,23 +125,23 @@ endfunction : setup_AhbMasterAgentConfig
 //--------------------------------------------------------------------------------------------
     
 function void AhbBaseTest::setup_AhbSlaveAgentConfig();
-  ahb_env_cfg_h.ahb_slave_agent_cfg_h = new[ahb_env_cfg_h.no_of_slaves];
-  foreach(ahb_env_cfg_h.ahb_slave_agent_cfg_h[i]) begin
-    ahb_env_cfg_h.ahb_slave_agent_cfg_h[i] = AhbSlaveAgentConfig::type_id::create($sformatf("AhbSlaveAgentConfig[%0d]",i));
-    ahb_env_cfg_h.ahb_slave_agent_cfg_h[i].slave_id       = i;
-    ahb_env_cfg_h.ahb_slave_agent_cfg_h[i].slave_selected = 0;
-    ahb_env_cfg_h.ahb_slave_agent_cfg_h[i].min_address    = ahb_env_cfg_h.ahb_master_agent_cfg_h.master_min_addr_range_array[i];
-    ahb_env_cfg_h.ahb_slave_agent_cfg_h[i].max_address    = ahb_env_cfg_h.ahb_master_agent_cfg_h.master_max_addr_range_array[i];
+  ahbEnvironmentConfig.ahb_slave_agent_cfg_h = new[ahbEnvironmentConfig.no_of_slaves];
+  foreach(ahbEnvironmentConfig.ahbSlaveAgentConfig[i]) begin
+    ahbEnvironmentConfig.ahbSlaveAgentConfig[i] = AhbSlaveAgentConfig::type_id::create($sformatf("AhbSlaveAgentConfig[%0d]",i));
+    ahbEnvironmentConfig.ahbSlaveAgentConfig[i].slave_id       = i;
+    ahbEnvironmentConfig.ahbSlaveAgentConfig[i].slave_selected = 0;
+    ahbEnvironmentConfig.ahbSlaveAgentConfig[i].min_address    = ahbEnvironmentConfig.ahbMasterAgentConfig.master_min_addr_range_array[i];
+    ahbEnvironmentConfig.ahbSlaveAgentConfig[i].max_address    = ahbEnvironmentConfig.ahbMasterAgentConfig.master_max_addr_range_array[i];
     if(SLAVE_AGENT_ACTIVE === 1) begin
-      ahb_env_cfg_h.ahb_slave_agent_cfg_h[i].is_active = uvm_active_passive_enum'(UVM_ACTIVE);
+      ahbEnvironmentConfig.ahbSlaveAgentConfig[i].is_active = uvm_active_passive_enum'(UVM_ACTIVE);
     end
     else begin
-      ahb_env_cfg_h.ahb_slave_agent_cfg_h[i].is_active = uvm_active_passive_enum'(UVM_PASSIVE);
+      ahbEnvironmentConfig.ahbSlaveAgentConfig[i].is_active = uvm_active_passive_enum'(UVM_PASSIVE);
     end
-    ahb_env_cfg_h.ahb_slave_agent_cfg_h[i].has_coverage = 1; 
+    ahbEnvironmentConfig.ahbSlaveAgentConfig[i].has_coverage = 1; 
     uvm_config_db #(AhbSlaveAgentConfig)::set(this,$sformatf("*env*"),$sformatf("AhbSlaveAgentConfig[%0d]",i),
-    ahb_env_cfg_h.ahb_slave_agent_cfg_h[i]);
-    `uvm_info(get_type_name(),$sformatf("\nAHB_SLAVE_CONFIG[%0d]\n%s",i,ahb_env_cfg_h.ahb_slave_agent_cfg_h[i].sprint()),UVM_LOW);
+    ahbEnvironmentConfig.ahbSlaveAgentConfig[i]);
+    `uvm_info(get_type_name(),$sformatf("\nAHB_SLAVE_CONFIG[%0d]\n%s",i,ahbEnvironmentConfig.ahbSlaveAgentConfig[i].sprint()),UVM_LOW);
   end
 
 endfunction : setup_AhbSlaveAgentConfig
