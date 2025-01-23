fc896bd880cf781a691aad87d31c1da3849c7660
Sonal J Ail <131892960+sonalail@users.noreply.github.com>
2025-01-23 23:33:12 +0530

    Update AhbVirtualBaseSequence.sv

--- src/hvlTop/tb/virtualSequences/AhbVirtualBaseSequence.sv
+++ src/hvlTop/tb/virtualSequences/AhbVirtualBaseSequence.sv
@@ -6,7 +6,7 @@
 // Class: AhbVirtualBaseSequence
 // Holds the handle of actual sequencer.
 //--------------------------------------------------------------------------------------------
-class AhbVirtualBaseSequence extends uvm_sequence; 
+class AhbVirtualBaseSequence extends uvm_sequence#(uvm_seq_item); 
   `uvm_object_utils(AhbVirtualBaseSequence)
   
   //Declaring p_sequencer
