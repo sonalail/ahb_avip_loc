be69398bb894835835262cd7f713ff8f34a0c600
Sonal J Ail <131892960+sonalail@users.noreply.github.com>
2025-01-23 10:56:02 +0530

    Update AhbMasterBaseSequence.sv

--- src/hvlTop/masterAgent/masterSequences/AhbMasterBaseSequence.sv
+++ src/hvlTop/masterAgent/masterSequences/AhbMasterBaseSequence.sv
@@ -21,10 +21,8 @@ class AhbMasterBaseSequence extends uvm_sequence#(uvm_sequence_item);
   //-------------------------------------------------------
   extern function new(string name = "AhbMasterBaseSequence");
   extern virtual function void build_phase(uvm_phase phase);
-  extern virtual function void end_of_elaboration_phase(uvm_phase phase);
-  extern virtual function void start_of_simulation_phase(uvm_phase phase);
-  extern virtual task run_phase(uvm_phase phase);
-
+  extern task body();  
+  
 endclass : AhbMasterBaseSequence
 
 //--------------------------------------------------------------------------------------------
@@ -38,60 +36,14 @@ function AhbMasterBaseSequence::new(string name = "AhbMasterBaseSequence");
   super.new(name);
 endfunction : new
 
-//--------------------------------------------------------------------------------------------
-// Function: build_phase
-// Called during the UVM build phase
-//
-// Parameters:
-//  phase - UVM phase
-//--------------------------------------------------------------------------------------------
-function void AhbMasterBaseSequence::build_phase(uvm_phase phase);
-  super.build_phase(phase);
-  // Add any initialization here, such as assigning sequencer handles
-endfunction : build_phase
+task AhbMasterBaseSequence::body();
 
-//--------------------------------------------------------------------------------------------
-// Function: end_of_elaboration_phase
-// Called at the end of the elaboration phase
-//
-// Parameters:
-//  phase - UVM phase
-//--------------------------------------------------------------------------------------------
-function void AhbMasterBaseSequence::end_of_elaboration_phase(uvm_phase phase);
-  super.end_of_elaboration_phase(phase);
-  // Add actions to be performed at the end of elaboration
-endfunction : end_of_elaboration_phase
-
-//--------------------------------------------------------------------------------------------
-// Function: start_of_simulation_phase
-// Called during the start of simulation
-//
-// Parameters:
-//  phase - UVM phase
-//--------------------------------------------------------------------------------------------
-function void AhbMasterBaseSequence::start_of_simulation_phase(uvm_phase phase);
-  super.start_of_simulation_phase(phase);
-  // Add actions to be performed at simulation start
-endfunction : start_of_simulation_phase
-
-//--------------------------------------------------------------------------------------------
-// Task: run_phase
-// Main phase for the sequence
-//
-// Parameters:
-//  phase - UVM phase
-//--------------------------------------------------------------------------------------------
-task AhbMasterBaseSequence::run_phase(uvm_phase phase);
-  `uvm_info("AHB BASE SEQUENCE", "AHB base sequence started", UVM_LOW)
-  
- 
-
-  // Add sequence logic here
-
-
-  
-endtask : run_phase
-
+  //dynamic casting of p_sequencer and m_sequencer
+  if(!$cast(p_sequencer,m_sequencer))begin
+    `uvm_error(get_full_name(),"Virtual sequencer pointer cast failed")
+  end
+            
+endtask : body
 `endif
 //------------------------------------------------------------------------------------------------
 
