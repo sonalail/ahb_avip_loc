2c334740c3181f22a4b3956d45ee45b75556ad5b
SHUBHA SHETTY <132587998+ShubhaSudeshShetty@users.noreply.github.com>
2025-01-24 11:55:34 +0530

    Update AhbBaseTest.sv

--- src/hvlTop/tb/test/AhbBaseTest.sv
+++ src/hvlTop/tb/test/AhbBaseTest.sv
@@ -1,27 +1,13 @@
 `ifndef AHBBASETEST_INCLUDED_
 `define AHBBASETEST_INCLUDED_
 
-
-//--------------------------------------------------------------------------------------------
-// Class: AhbBaseTest
-//  Base test has the testcase scenarios for the tesbench
-//  Env and Config are created in AhbBaseTest
-//  Sequences are created and started in the test
-//--------------------------------------------------------------------------------------------
 class AhbBaseTest extends uvm_test;
   `uvm_component_utils(AhbBaseTest)
   
-  //Variable: ahbEnvironment
-  //Declaring a handle for AhbEnvironment
-  AhbEnvironment ahbEnvironment;
+   AhbEnvironment ahbEnvironment;
 
-  //Variable: ahbEnvironmentConfig
-  //Declaring a handle for AhbEnvironmentConfig
-  AhbEnvironmentConfig ahbEnvironmentConfig;
+   AhbEnvironmentConfig ahbEnvironmentConfig;
 
-  //-------------------------------------------------------
-  // Externally defined Tasks and Functions
-  //-------------------------------------------------------
   extern function new(string name = "AhbBaseTest", uvm_component parent = null);
   extern virtual function void build_phase(uvm_phase phase);
   extern virtual function void setupAhbEnvironmentConfig();
@@ -32,46 +18,25 @@ class AhbBaseTest extends uvm_test;
 
 endclass : AhbBaseTest
 
-//--------------------------------------------------------------------------------------------
-// Construct: new
-//
-// Parameters:
-//  name - AhbBaseTest
-//  parent - parent under which this component is created
-//--------------------------------------------------------------------------------------------
 function AhbBaseTest::new(string name = "AhbBaseTest",uvm_component parent = null);
   super.new(name, parent);
 endfunction : new
 
-//--------------------------------------------------------------------------------------------
-// Function: build_phase
-//  Creates env and required configuarions
-//
-// Parameters:
-//  phase - uvm phase
-//--------------------------------------------------------------------------------------------
 function void AhbBaseTest::build_phase(uvm_phase phase);
   super.build_phase(phase);
   setupAhbEnvironmentConfig();
   ahbEnvironment = AhbEnvironment::type_id::create("ahbEnvironment",this);
 endfunction : build_phase
 
-//--------------------------------------------------------------------------------------------
-// Function : setup_AhbEnvironmentConfig
-//  It calls the master agent config setup and slave agent config steup functions
-//--------------------------------------------------------------------------------------------
 function void AhbBaseTest::setupAhbEnvironmentConfig();
   ahbEnvironmentConfig = AhbEnvironmentConfig::type_id::create("ahbEnvironmentConfig");
   ahbEnvironmentConfig.noOfSlaves      = NO_OF_SLAVES;
   ahbEnvironmentConfig.hasScoreboard    = 1;
   ahbEnvironmentConfig.hasVirtualSequencer  = 1;
 
-  //Setting up the configuration for master agent
-  setupAhbMasterAgentConfig();
+   setupAhbMasterAgentConfig();
 
-  //Setting the master agent configuration into config_db
-  uvm_config_db#(AhbMasterAgentConfig)::set(this,"*MasterAgent*","AhbMasterAgentConfig",ahbEnvironmentConfig.ahbMasterAgentConfig);
- //Displaying the master agent configuration
+   uvm_config_db#(AhbMasterAgentConfig)::set(this,"*MasterAgent*","AhbMasterAgentConfig",ahbEnvironmentConfig.ahbMasterAgentConfig);
   `uvm_info(get_type_name(),$sformatf("\nAHB_MASTER_AGENT_CONFIG\n%s",ahbEnvironmentConfig.ahbMasterAgentConfig.sprint()),UVM_LOW);
  
 // ahbEnvironmentConfig.ahbSlaveAgentConfig = new[ahbEnvironmentConfig.ahbSlaveAgentConfig.noOfSlaves];
@@ -85,26 +50,30 @@ function void AhbBaseTest::setupAhbEnvironmentConfig();
   `uvm_info(get_type_name(),$sformatf("\nAHB_ENV_CONFIG\n%s",ahbEnvironmentConfig.sprint()),UVM_LOW);
 
 endfunction : setupAhbEnvironmentConfig
-//--------------------------------------------------------------------------------------------
-// Function : setupAhbMasterAgentConfig
-//  Sets the configurations to the corresponding variables in ahb master agent config
-//  Creates the master agent config
-//  Sets ahb master agent config into configdb 
-//--------------------------------------------------------------------------------------------
+
 function void AhbBaseTest::setupAhbMasterAgentConfig();
   // bit [63:0]local_min_address;
   // bit [63:0]local_max_address;
   
-  ahbEnvironmentConfig.ahbMasterAgentConfig = AhbMasterAgentConfig::type_id::create("AhbMasterAgentConfig");
-  
-  if(MASTER_AGENT_ACTIVE === 1) begin
-    ahbEnvironmentConfig.ahbMasterAgentConfig.is_active = uvm_active_passive_enum'(UVM_ACTIVE);
+  ahbEnvironmentConfig.ahbMasterAgentConfig = new[ahbEnvironmentConfig.noOfMasters];
+  foreach(ahbEnvironmentConfig.ahbMAsterAgentConfig[i]) begin
+    ahbEnvironmentConfig.ahbMasterAgentConfig[i] = AhbMasterAgentConfig::type_id::create($sformatf("AhbMasterAgentConfig[%0d]",i));
+ 
+   if(MASTER_AGENT_ACTIVE === 1) begin
+     ahbEnvironmentConfig.ahbMasterAgentConfig[i].is_active = uvm_active_passive_enum'(UVM_ACTIVE);
   end
   else begin
-    ahbEnvironmentConfig.ahbMasterAgentConfig.is_active = uvm_active_passive_enum'(UVM_PASSIVE);
+    ahbEnvironmentConfig.ahbMasterAgentConfig[i].is_active = uvm_active_passive_enum'(UVM_PASSIVE);
   end
-  ahbEnvironmentConfig.ahbMasterAgentConfig.noOfSlaves = NO_OF_SLAVES;
-  ahbEnvironmentConfig.ahbMasterAgentConfig.hasCoverage = 1;
+
+   ahbEnvironmentConfig.ahbMasterAgentConfig[i].hasCoverage = 1; 
+  uvm_config_db #(AhbMasterAgentConfig)::set(this,$sformatf("*env*"),$sformatf("AhbMasterAgentConfig[%0d]",i),
+    ahbEnvironmentConfig.ahbMasterAgentConfig[i]);
+  `uvm_info(get_type_name(),$sformatf("\nAHB_MASTER_CONFIG[%0d]\n%s",i,ahbEnvironmentConfig.ahbMasterAgentConfig[i].sprint()),UVM_LOW);
+   end
+
+ // ahbEnvironmentConfig.ahbMasterAgentConfig.noOfSlaves = NO_OF_SLAVES;
+  
 
   // for(int i =0; i<NO_OF_SLAVES; i++) begin
   //   if(i == 0) begin  
@@ -123,11 +92,7 @@ function void AhbBaseTest::setupAhbMasterAgentConfig();
   //   end
   // end
 endfunction : setupAhbMasterAgentConfig
-//--------------------------------------------------------------------------------------------
-// Function : setup_AhbSlaveAgentConfig
-//  It calls the master agent config setup and slave agent config steup functions
-//--------------------------------------------------------------------------------------------
-    
+
 function void AhbBaseTest::setupAhbSlaveAgentConfig();
  ahbEnvironmentConfig.ahbSlaveAgentConfig = new[ahbEnvironmentConfig.noOfSlaves];
    foreach(ahbEnvironmentConfig.ahbSlaveAgentConfig[i]) begin
@@ -150,25 +115,11 @@ function void AhbBaseTest::setupAhbSlaveAgentConfig();
 
 endfunction : setupAhbSlaveAgentConfig
 
-//--------------------------------------------------------------------------------------------
-// Function: end_of_elaboration_phase
-//  Used to print topology
-//
-// Parameters:
-//  phase - uvm phase
-//--------------------------------------------------------------------------------------------
 function void AhbBaseTest::end_of_elaboration_phase(uvm_phase phase);
   super.end_of_elaboration_phase(phase);
   uvm_top.print_topology();
 endfunction  : end_of_elaboration_phase
 
-//--------------------------------------------------------------------------------------------
-// Task: run_phase
-//  Used to give 100ns delay to complete the run_phase.
-//
-// Parameters:
-//  phase - uvm phase
-//--------------------------------------------------------------------------------------------
 task AhbBaseTest::run_phase(uvm_phase phase);
   `uvm_info(get_type_name(),$sformatf("Inside run phase of test"),UVM_LOW);
   phase.raise_objection(this);
