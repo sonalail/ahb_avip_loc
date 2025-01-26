9f1271a55ab3d3aa7887e03a750e816f10e131bf
Sonal J Ail <131892960+sonalail@users.noreply.github.com>
2025-01-26 23:24:42 +0530

    Update HdlTop.sv

--- src/hdlTop/HdlTop.sv
+++ src/hdlTop/HdlTop.sv
@@ -1,48 +1,26 @@
 `ifndef HDLTOP_INCLUDED
 `define HDLTOP_INCLUDED
 
-//--------------------------------------------------------------------------------------------
-// Module      : HDL Top
-// Description : Has a interface and slave agent bfm.
-//--------------------------------------------------------------------------------------------
 module HdlTop;
 
-  //-------------------------------------------------------
-  // Importing uvm package and Including uvm macros file
-  //-------------------------------------------------------
   import uvm_pkg::*;
   `include "uvm_macros.svh"
-  
-  //-------------------------------------------------------
-  // Importing apb global package
-  //-------------------------------------------------------
+
   import AhbGlobalPackage::*;
 
   initial begin
     `uvm_info("HDL_TOP","HDL_TOP",UVM_LOW);
   end
 
-  //Variable : hclk
-  //Declaration of system clock
   bit hclk;
 
-  //Variable : hresetn
-  //Declaration of system reset
   bit hresetn;
 
-  //-------------------------------------------------------
-  // Generation of system clock at frequency rate of 20ns
-  //-------------------------------------------------------
   initial begin
    hclk = 1'b0;
     forever #10 hclk =!hclk;
   end
 
-  //-------------------------------------------------------
-  // Generation of system reset signal,
-  //  system reset can be asserted asynchronously,
-  //  but system reset de-assertion is synchronous.
-  //-------------------------------------------------------
   initial begin
     hresetn = 1'b1;
     #15 hresetn= 1'b0;
@@ -53,19 +31,10 @@ module HdlTop;
     hresetn = 1'b1;
   end
 
-  //-------------------------------------------------------
-  // AHB Interface Instantiation
-  //-------------------------------------------------------
-  AhbInterface ahbInterface(hclk,hresetn);
+ AhbInterface ahbInterface(hclk,hresetn);
 
-  //-------------------------------------------------------
-  // AHB Master BFM Agent Instantiation
-  //-------------------------------------------------------
-  AhbMasterAgentBFM ahbMasterAgentBFM(ahbInterface); 
-  
-  //-------------------------------------------------------
-  // AHB Slave BFM Agent Instantiation
-  //-------------------------------------------------------
+ AhbMasterAgentBFM ahbMasterAgentBFM(ahbInterface); 
+
  AhbSlaveAgentBFM ahbSlaveAgentBFM(ahbInterface); 
 
 endmodule : HdlTop
