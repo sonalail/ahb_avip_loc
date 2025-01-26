2a4ba3097425b6098c9c925033b9d4acce841c74
Sonal J Ail <131892960+sonalail@users.noreply.github.com>
2025-01-26 23:15:51 +0530

    Update AhbMasterMonitorBFM.sv

--- src/hdlTop/masterAgentBFM/AhbMasterMonitorBFM.sv
+++ src/hdlTop/masterAgentBFM/AhbMasterMonitorBFM.sv
@@ -2,15 +2,8 @@
 `ifndef AHBMASTERMONITORBFM_INCLUDED_
 `define AHBMASTERMONITORBFM_INCLUDED_
 
-//-------------------------------------------------------
-// Importing ahb global package
-//-------------------------------------------------------
 import AhbGlobalPackage::*;
 
-//--------------------------------------------------------------------------------------------
-// Interface: AhbMasterMonitorBFM
-//  Connects the master monitor bfm with the master monitor proxy
-//--------------------------------------------------------------------------------------------
 interface AhbMasterMonitorBFM(input  bit   hclk,
                               input  bit  hresetn,
     input logic [ADDR_WIDTH-1:0] haddr,
@@ -32,33 +25,20 @@ interface AhbMasterMonitorBFM(input  bit   hclk,
     input logic hexokay,
     input logic hready
 );
-  //-------------------------------------------------------
-  // Importing uvm package and AhbMasterPackage file
-  //-------------------------------------------------------
+
   import uvm_pkg::*;
   `include "uvm_macros.svh"
 
-  //-------------------------------------------------------
-  // Importing global package
-  //-------------------------------------------------------
   import AhbMasterPackage::*;
 
-  // Variable: ahbMasterMonitorProxy
-  // Declaring handle for AhbMasterMonitorProxy  
   AhbMasterMonitorProxy ahbMasterMonitorProxy;
 
-  // Variable: name
-  // Assigning the string used in infos
   string name = "AHB_MASTER_MONITOR_BFM"; 
  
   initial begin
     `uvm_info(name, $sformatf("AHB MASTER MONITOR BFM"), UVM_LOW);
   end
 
-  //-------------------------------------------------------
-  // Task: waitForResetn
-  //  Waiting for the system reset to be active low
-  //-------------------------------------------------------
   task waitForResetn();
       @(negedge hresetn);
     `uvm_info(name, $sformatf("system reset detected"), UVM_HIGH)
@@ -66,16 +46,8 @@ interface AhbMasterMonitorBFM(input  bit   hclk,
     @(posedge hresetn);
     `uvm_info(name, $sformatf("system reset deactivated"), UVM_HIGH)
   endtask : waitForResetn
-  
-  //-------------------------------------------------------
-  // Task: sampleData
-  //  In this task, the HWDATA and HRDATA is sampled
-  //
-  // Parameters: 
-  //  ahbDataPacket - Handle for ahbTransferCharStruct class
-  //  ahbConfigPacket  - Handle for ahbTransferConfigStruct class
-  //-------------------------------------------------------
-    task sampleData (output ahbTransferCharStruct ahbDataPacket, input ahbTransferConfigStruct ahbConfigPacket);
+
+  task sampleData (output ahbTransferCharStruct ahbDataPacket, input ahbTransferConfigStruct ahbConfigPacket);
    //logic to be written
     
   endtask : sampleData
