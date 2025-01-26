f90dae9f191a03b7fbf15ebf49d31792529e8614
Sonal J Ail <131892960+sonalail@users.noreply.github.com>
2025-01-26 23:26:45 +0530

    Update HvlTop.sv

--- src/hvlTop/HvlTop.sv
+++ src/hvlTop/HvlTop.sv
@@ -1,21 +1,11 @@
 `ifndef HVLTOP_INCLUDED_
 `define HVLTOP_INCLUDED_
 
-//--------------------------------------------------------------------------------------------
-// Module : HvlTop
-//  Starts the testbench components
-//--------------------------------------------------------------------------------------------
 module HvlTop;
 
-  //-------------------------------------------------------
-  // Importing UVM Package and test Package
-  //-------------------------------------------------------
   import uvm_pkg::*;
   import AhbBaseTestPackage::*;
-  
-  //-------------------------------------------------------
-  // Calling run_test for simulation
-  //-------------------------------------------------------
+
   initial begin
     run_test("AhbBaseTest");
   end
