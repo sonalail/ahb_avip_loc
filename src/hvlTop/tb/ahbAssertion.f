86af00dd31e7fe3ceb1f3b4fe8269d888f92dc68
Zishan Manna <139680050+ZishanManna@users.noreply.github.com>
2025-01-29 15:39:58 +0530

    Create ahbAssertion.f

---
+++ src/hvlTop/tb/ahbAssertion.f
@@ -0,0 +1,12 @@
++incdir+($basepath)/tb/
++incdir+($basepath)/ahbMasterAgent/
++incdir+($basepath)/ahbSlaveAgent/
++incdir+($basepath)/tb/ahbLocalAssertionTest/
++incdir+src/globals/
+
+$basepath/ahbMasterAgent/AhbMasterCoverParameter.sv
+$basepath/ahbSlaveAgent/AhbSlaveCoverParameter.sv
+$basepath/ahbMasterAgent/AhbMasterAssertions.sv
+$basepath/ahbSlaveAgent/AhbSlaveAssertions.sv
+$basepath/tb/ahbLocalAssertionTest/AhbMasterAssertionTB.sv
+$basepath/tb/ahbLocalAssertionTest/AhbSlaveAssertionTB.sv
