ae33d250e3505bdcf6241e24f449bedfe2cdc171
prakhyath06 <159763985+prakhyath06@users.noreply.github.com>
2025-02-03 12:05:39 +0530

    Updated assertion task

--- src/hvlTop/tb/AhbSlaveAssertionTb.sv
+++ src/hvlTop/tb/AhbSlaveAssertionTb.sv
@@ -50,28 +50,54 @@ module AhbSlaveAssertionTb;
 
   // Initial block to apply stimulus and reset
   initial begin
-    // Initialize signals
     hclk = 0;
-    hresetn = 0;
-    hreadyout = 0;
-    hrdata = 32'b0;
-    hresp = 2'b00;
-    haddr = 32'b0;
-    htrans = 2'b00;
-    hwrite = 1'b0;
-    hsize = 3'b010;  // Word transfer
-    hburst = 3'b000; // Single burst
-    hselx = 1'b0;
-    hwdata = 32'b0;
-    hprot = 4'b0000;
-    hexokay = 1'b0;
-    hwstrb = 4'b1111;
+    hrsetn = 0;
+    #15 hrsetn = 1;
+  end
 
-    // Apply reset
-    #10 hresetn = 1'b1;
+  initial begin
+    InitialSignals();
+    ValidReadTransaction();
+    ValidReadTransactionFail();
+    InvalidReadwithHRESPError();
+    WriteTransactionPassAssertion();
+    InvalidBurstTypeFail();
+    BurstTransactionIncrement();
+    InvalidBurstType();
+    IdleStatewithHrespErrorFail();
+    IdleStatewithHrsepError();
+    HsizeDoesNotMatcheDataWidthFail();
+    HrespIsOkayForValidTransactionFail();
+  end
+  
+
+  // Initialize signals
+  task InitialSignals();
+    `uvm_info(name,$sformatf("When reset is high Initialize the signals "),UVM_NONE);
+     @(posedge hclk);
+     if(hresetn==0)
+       begin
+         hreadyout = 0;
+         hrdata = 32'b0;
+         hresp = 2'b00;
+         haddr = 32'b0;
+         htrans = 2'b00;
+         hwrite = 1'b0;
+         hsize = 3'b010;  // Word transfer
+         hburst = 3'b000; // Single burst
+         hselx = 1'b0;
+         hwdata = 32'b0;
+         hprot = 4'b0000;
+         hexokay = 1'b0
+         hwstrb = 4'b1111;
+       end
+     `uvm_info(name,$sformatf("When_arvalidIsAsserted_Then_sameClkAraddrIsNotUnknown_Expect_AssertionPass Task Ended"),UVM_NONE);
+   endtask
 
     // Test 1: Valid Read Transaction
-    #10;
+  task ValidReadTransaction();
+    `uvm_info(name,$sformatf("Valid Read Transaction Started "),UVM_NONE);
+    @(posedge hclk);
     htrans = 2'b10;  // NONSEQ transaction
     hreadyout = 1;
     hwrite = 0;      // Read
@@ -79,9 +105,13 @@ module AhbSlaveAssertionTb;
     haddr = 32'h0000_1000;   // Example address
     hresp = 2'b00;   // OKAY response
     #20;
-    
-    // Test 1.1: Invalid Read Transaction(Fail Assertion)
-    #10;
+    `uvm_info(name,$sformatf("Valid Read Transaction Ended "),UVM_NONE);
+  endtask
+
+  // Test 1.1: Invalid Read Transaction(Fail Assertion)
+  task ValidReadTransactionFail();
+    `uvm_info(name,$sformatf("Invalid Read Transaction (Fail Assertion) Started "),UVM_NONE);
+    @(posedge hclk);
     htrans = 2'b10;  // NONSEQ transaction
     hreadyout = 1;
     hwrite = 0;      // Read
@@ -89,10 +119,24 @@ module AhbSlaveAssertionTb;
     haddr = 32'h0000_1000;   // Example address
     hresp = 2'b00;   // OKAY response
     #20;
+    `uvm_info(name,$sformatf("Invalid Read Transaction (Fail Assertion) Ended "),UVM_NONE);
+  endtask
+  
+    // Test 2: Invalid Read with HRESP Error
+  task InvalidReadwithHRESPError();
+    `uvm_info(name,$sformatf("Invalid Read with HRESP Error Started "),UVM_NONE);
+    @(posedge hclk);
+    htrans = 2'b00;  // IDLE
+    hreadyout = 1;
+    hresp = 2'b01;   // ERROR response
+    #20;
+    `uvm_info(name,$sformatf("Invalid Read with HRESP Error Ended"),UVM_NONE);
+  endtask
     
-
-    // Test 2: Write Transaction (Pass Assertion)
-    #10;
+  // Test 3: Write Transaction (Pass Assertion)
+  task WriteTransactionPassAssertion();
+    `uvm_info(name,$sformatf("Write Transaction Pass Assertion Started "),UVM_NONE);
+    @(posedge hclk);
     htrans = 2'b01;  // BUSY transaction
     hreadyout = 1;
     hwrite = 1;      // Write
@@ -100,72 +144,94 @@ module AhbSlaveAssertionTb;
     haddr = 32'h0000_2000;   // Example address
     hresp = 2'b00;   // OKAY response
     #20;
-
-
-    // Test 3: Burst Transaction (INCR)
-    #10;
+    `uvm_info(name,$sformatf("Write Transaction Pass Assertion Ended"),UVM_NONE);
+  endtask
+    
+  
+  // Test 3.1: Invalid Burst Type (Fail Assertion)
+  task InvalidBurstTypeFail();
+    `uvm_info(name,$sformatf("Invalid Burst Type Fail Started "),UVM_NONE);
+    @(posedge hclk);
+    htrans = 2'b10;  // NONSEQ
+    hburst = 3'b101; // Invalid burst type
+    hreadyout = 1;
+    hresp = 2'b00;
+    #20;
+    `uvm_info(name,$sformatf("Invalid Burst Type Fail Ended"),UVM_NONE);
+  endtask
+  
+  
+  // Test 4: Burst Transaction (INCR)
+  task BurstTransactionIncrement();
+    `uvm_info(name,$sformatf("Burst Transaction (INCR) Started "),UVM_NONE);
+    @(posedge hclk);
     htrans = 2'b10;  // NONSEQ
     hburst = 3'b001; // INCR burst
     hreadyout = 1;
     haddr = 32'h0000_3000;
     hresp = 2'b00;
     #20;
-
-    // Test 3.1: Invalid Burst Type (Fail Assertion)
-    #10;
+    `uvm_info(name,$sformatf("Burst Transaction (INCR) Ended"),UVM_NONE);
+  endtask
+   
+  
+  // Test 4.1: Idle State with HRESP ERROR(Fail assertion)
+  task IdleStatewithHrespErrorFail();
+    `uvm_info(name,$sformatf("Idle State with HRESP ERROR Fail Started "),UVM_NONE);
+    @(posedge hclk);
+    htrans = 2'b00;  // IDLE
+    hreadyout = 1;
+    hresp = 2'b00;   // OKAY  response
+    #20;
+    `uvm_info(name,$sformatf("Idle State with HRESP ERROR Fail Ended"),UVM_NONE);
+  endtask
+  
+  // Test 5: Invalid Burst Type
+  task InvalidBurstType();
+    `uvm_info(name,$sformatf("Invalid Burst Type Started "),UVM_NONE);
+    @(posedge hclk);
     htrans = 2'b10;  // NONSEQ
     hburst = 3'b101; // Invalid burst type
     hreadyout = 1;
     hresp = 2'b00;
     #20;
-
-    // Test 4: Idle State with HRESP ERROR
-    #10;
-    htrans = 2'b00;  // IDLE
-    hreadyout = 1;
-    hresp = 2'b01;   // ERROR response
-    #20;
-   
-    // Test 4.1: Idle State with HRESP ERROR(Fail assertion)
-    #10;
-    htrans = 2'b00;  // IDLE
-    hreadyout = 1;
-    hresp = 2'b00;   // OKAY  response
-    #20;
-    
-    //Test 5 :HSIZE matches data width
-    #10;
-    hreadyout = 1;
-    htrans = 2'b10; //NONSEQ
-    hsize = 3'b101;
-    #20;
-    
-   //Test 5.1 :HSIZE does not matche data width)(Fail Assertion)
-    #10;
+    `uvm_info(name,$sformatf("Invalid Burst Type Ended"),UVM_NONE);
+  endtask
+  
+  
+  //Test 5.1 :HSIZE does not matche data width)(Fail Assertion)
+  task HsizeDoesNotMatcheDataWidthFail();
+    `uvm_info(name,$sformatf("HSIZE does not matche data width Fail Started "),UVM_NONE);
+    @(posedge hclk);
     hreadyout = 1;
     htrans = 2'b10; //NONSEQ
     hsize = 3'b110;
     #20;
-    
-    //Test 6 :HRESP is OKAY for valid transaction (Pass assertion)
-    #10;
-    hreadyout = 1; // Ready signal is high
-    htrans = 2'b10; // NONSEQ (valid transaction)
-    hresp = 1'b0; // OKAY response
+    `uvm_info(name,$sformatf("HSIZE does not matche data width Fail Ended"),UVM_NONE);
+  endtask
+  
+  // Test 6: Idle State with HRESP ERROR
+  task IdleStatewithHrsepError();
+    `uvm_info(name,$sformatf("Idle State with HRESP ERROR Started "),UVM_NONE);
+    @(posedge hclk);
+    htrans = 2'b00;  // IDLE
+    hreadyout = 1;
+    hresp = 2'b01;   // ERROR response
     #20;
+   `uvm_info(name,$sformatf("Idle State with HRESP ERROR Ended"),UVM_NONE);
+  endtask
    
-    //Test 6.1 :HRESP is OKAY for valid transaction (Fail assertion)
-    #10;
+   //Test 6.1 :HRESP is OKAY for valid transaction (Fail assertion)
+  task HrespIsOkayForValidTransactionFail();
+    `uvm_info(name,$sformatf("HRESP is OKAY for valid transaction Fail Started "),UVM_NONE);
+    @(posedge hclk);
     hreadyout = 1; // Ready signal is high
     htrans = 2'b10; // NONSEQ (valid transaction)
     hresp = 1'b1; // ERROR response
     #20;       
-     
-    
-    // End simulation
-    $finish;
-  end
-
+    `uvm_info(name,$sformatf("HRESP is OKAY for valid transaction Fail Ended"),UVM_NONE);
+  endtask
+  
   // Monitor the assertion errors
   initial begin
     $monitor("Time=%0t, htrans=%b, hresp=%b, hreadyout=%b, hrdata=%h, haddr=%h, hwrite=%b, hburst=%b",
@@ -174,3 +240,4 @@ module AhbSlaveAssertionTb;
 
 endmodule
 `endif
+
