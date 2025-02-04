97a5294b70a0d3ee97a3188a43dd6a9ede1f4096
Nisha Shetty <109810083+nishashetty11@users.noreply.github.com>
2025-02-04 11:49:15 +0530

    Update AhbCoverPropertyTb.sv

--- src/hvlTop/tb/AhbCoverPropertyTb.sv
+++ src/hvlTop/tb/AhbCoverPropertyTb.sv
@@ -30,28 +30,29 @@ module AhbCoverPropertyTb;
   reg [DATA_WIDTH-1:0]  hwdataValid;
   string name = "AhbCoverPropertyTb";
 
-AhbCoverProperty ahbcoverproperty_u(input hclk,
-                            input hresetn,
-                            input [ADDR_WIDTH-1:0] haddr, 
-                            input [NO_OF_SLAVES-1:0] hselx;
-                            input [2:0] hburst;
-                            input hmastlock;
-                            input [HPROT_WIDTH-1:0] hprot;
-                            input [2:0] hsize;
-                            input hnonsec;
-                            input hexcl;
-                            input [HMASTER_WIDTH-1:0] hmaster;
-                            input [1:0] htrans;
-                            input [DATA_WIDTH-1:0] hwdata;
-                            input [(DATA_WIDTH/8)-1:0] hwstrb;
-                            input hwrite;
-                            input [DATA_WIDTH-1:0] hrdata;
-                            input hreadyout;
-                            input hresp;
-                            input hexokay;
-                            input hready;
-                            input htransValid;
-                            input [DATA_WIDTH-1:0] hwdataValid;
+ AhbCoverProperty ahbcoverproperty_u(
+  .hclk(hclk),
+  .hresetn(hresetn),
+  .haddr(haddr), 
+  .hselx(hselx),
+  .hburst(hburst),
+  .hmastlock(hmastlock),
+  .hprot(hprot),
+  .hsize(hsize),
+  .hnonsec(hnonsec);
+  .hexcl(hexcl),
+  .hmaster(hmaster),
+  .htrans(htrans),
+  .hwdata(hwdata),
+  .hwstrb(hwstrb),
+  .hwrite(hwrite),
+  .hrdata(hrdata),
+  .hreadyout(hreadyout),
+  .hresp(hresp),
+  .hexokay(hexokay)
+  .hready(hready),
+  .htransValid(htransValid),
+  .hwdataValid(hwdataValid)
                            );
 //-------------------------------------------------------
 // Clock Generation
