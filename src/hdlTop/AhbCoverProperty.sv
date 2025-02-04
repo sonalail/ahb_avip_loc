94e3c2e68de29971fc0ffc03d28ebd1a73a93446
Nisha Shetty <109810083+nishashetty11@users.noreply.github.com>
2025-02-04 11:53:51 +0530

    Update AhbCoverProperty.sv

--- src/hdlTop/AhbCoverProperty.sv
+++ src/hdlTop/AhbCoverProperty.sv
@@ -3,28 +3,28 @@
 
 import AhbGlobalPackage::*;
 
-interface AhbCoverProperty (input hclk;
-                            input hresetn;
-                            input [ADDR_WIDTH-1:0] haddr;
-                            input [NO_OF_SLAVES-1:0] hselx;
-                            input [2:0] hburst;
-                            input hmastlock;
-                            input [HPROT_WIDTH-1:0] hprot;
-                            input [2:0] hsize;
-                            input hnonsec;
-                            input hexcl;
-                            input [HMASTER_WIDTH-1:0] hmaster;
-                            input [1:0] htrans;
-                            input [DATA_WIDTH-1:0] hwdata;
-                            input [(DATA_WIDTH/8)-1:0] hwstrb;
-                            input hwrite;
-                            input [DATA_WIDTH-1:0] hrdata;
-                            input hreadyout;
-                            input hresp;
-                            input hexokay;
-                            input hready;
-                            input htransValid;
-                            input [DATA_WIDTH-1:0] hwdataValid;
+interface AhbCoverProperty (input hclk,
+                            input hresetn,
+                            input [ADDR_WIDTH-1:0] haddr,
+                            input [NO_OF_SLAVES-1:0] hselx,
+                            input [2:0] hburst,
+                            input hmastlock,
+                            input [HPROT_WIDTH-1:0] hprot,
+                            input [2:0] hsize,
+                            input hnonsec,
+                            input hexcl,
+                            input [HMASTER_WIDTH-1:0] hmaster,
+                            input [1:0] htrans,
+                            input [DATA_WIDTH-1:0] hwdata,
+                            input [(DATA_WIDTH/8)-1:0] hwstrb,
+                            input hwrite,
+                            input [DATA_WIDTH-1:0] hrdata,
+                            input hreadyout,
+                            input hresp,
+                            input hexokay,
+                            input hready,
+                            input htransValid,
+                            input [DATA_WIDTH-1:0] hwdataValid
                            );
   
   import uvm_pkg::*;
