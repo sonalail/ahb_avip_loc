970831c8f8ff323dbd47293293bde7ce75d359b0
prakhyath06 <159763985+prakhyath06@users.noreply.github.com>
2025-02-04 11:22:04 +0530

    deleted make file

---
+++ src/hvlTop/tb/ahbCoverProperty.f
@@ -0,0 +1,7 @@
++incdir+$basepath/hvlTop/tb/
++incdir+$basepath/hdlTop/
++incdir+$basepath/globals/
+
+$basepath/globals/AhbGlobalPackage.sv
+$basepath/hdlTop/AhbCoverProperty.sv
+$basepath/hvlTop/tb/AhbCoverPropertyTb.sv
